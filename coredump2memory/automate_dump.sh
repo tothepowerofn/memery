7b15e3884bf6807e740728e22c7405bb9ad4419a
tothepowerofn <nathanedu@icloud.com>
2019-11-19 16:32:25 -0500

    Update automate_dump.sh

--- coredump2memory/automate_dump.sh
+++ coredump2memory/automate_dump.sh
@@ -2,3 +2,12 @@
 process_to_get=$1
 process_id=`pidof $process_to_get`
 echo $process_id
+
+addrs=`python3 dump_addrs.py $process_id`
+addr_list=( $addrs )
+addr_start=${addr_list[0]}
+addr_end=${addr_list[1]}
+echo $addr_start
+echo $addr_end
+
+gdb -p $process_id -ex 'dump binary memory ~/dump.bin $addr_start $addr_end'
