7d8e822814ac11a054f15e7b5eac42bb05faf2f8
tothepowerofn <nathanedu@icloud.com>
2019-12-01 18:02:16 -0500

    edited dump script to work in new folder

---
+++ bin/dump_addrs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from sys import argv, exit
+
+
+def print_usage():
+	"""Print the usage string if script was used improperly"""
+	print('Usage: \
+		\t$ {} <pid>'.format(argv[0]))
+	exit(1)
+
+
+def read_write_heap(pid):
+	"""Find @read_str in the heap of @pid and replace it with @write_str"""
+	try:
+		maps_file = open("/proc/{}/maps".format(pid), 'r')
+	except IOError as e:
+		print("Can't open file /proc/{}/maps: IOError: {}".format(pid, e))
+		exit(1)
+	heap_info = None
+	for line in maps_file:
+		if 'heap' in line:
+			heap_info = line.split()
+	maps_file.close()
+	if 'heap' is None:
+		print('No heap found!')
+		exit(1)
+	addr = heap_info[0].split('-')
+	heap_start = addr[0]
+	heap_end = addr[1]
+	print("0x" + heap_start)
+	print("0x" + heap_end)
+	return heap_start, heap_end
+
+
+if (len(argv) == 2):
+	pid = argv[1]
+	read_write_heap(pid)
+else:
+	print_usage()
