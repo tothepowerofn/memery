64a6d933b475fcbbd59e0e7d7731ce2ceef97305
tothepowerofn <nathanedu@icloud.com>
2019-12-02 21:32:40 -0500

    non-consecutive and scattered tests added

--- tests/data_structures.c
+++ tests/data_structures.c
@@ -1,4 +1,5 @@
 #include "data_structures.h"
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -39,6 +40,7 @@ void simple_linked_list_test(int list_size, int num_lists){
 }
 
 //freq_bogus is a parameter in [1,inf) specifying how often to bogus nodes. Greater = more bogus
+//Note: nodes are still consecutive, but at variable spacing
 void scattered_linked_list(int list_size, int num_lists, int freq_bogus){
 	if(freq_bogus < 1){
 		printf("Bogus frequency must be greater than 1!\n");
@@ -58,8 +60,52 @@ void scattered_linked_list(int list_size, int num_lists, int freq_bogus){
 	}
 }
 
+void non_consecutive_list(int list_size){ //makes a linked list whereby nodes are non-consecutive
+	// Make nodes
+	int* used = (int*)malloc(sizeof(int)*list_size);
+	struct linked_list** nodes = (struct linked_list**) malloc(sizeof(struct linked_list*)*list_size);
+	for(int i = 0; i<list_size; ++i){
+		nodes[i] = (struct linked_list*)malloc(sizeof(struct linked_list*));
+		used[i] = 0;
+	}
+	int i = 0;
+	int index = random_int(0,list_size - 1);
+	printf("Order of nodes goes as follows:\n");
+	while(i < list_size-1){
+		int random_index = random_int(0,list_size - 1);
+		if(used[random_index] == 1 || random_index == index){ //this is stupid, needs to be more efficient
+			continue;
+		}
+		printf("%d (%p)\n", index, nodes[index]);
+
+		nodes[index]->next = nodes[random_index];
+		used[random_index] = 1;
+		used[index] = 1;
+		index = random_index;
+		++i;
+	}
+	printf("%d (%p)\n", index, nodes[index]);
+	printf("Now sleeping so that you can run automate_dump.sh!\n");
+}
+
+void simple_cyclic_list(int list_size){ //does a simple chain of nodes but links the last node back to the first
+	struct linked_list *list = NULL;
+	struct linked_list *head = NULL;
+	for (int j = 0; j < list_size; j++) {
+		list = linked_list_append_before(66, list);
+		if(head == NULL){
+			head = list;
+		}
+	}
+	list->next = head; //link the last node back to the first
+}
+
 int main() {
-	//Make a linked list with non-consecutive nodes
-	scattered_linked_list(20,1,3);
+	//Seed the random number generator so that it doesn't do the same sequence every time
+	srand(time(NULL)); 
+	//Make a linked list with variably spaced nodes
+	//scattered_linked_list(20,1,3);
+	//Make a list of non-consecutive nodes
+	non_consecutive_list(10);
 	sleep(10000);
 }
