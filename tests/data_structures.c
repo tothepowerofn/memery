bc39f3b25072015b696e233b0236f68cc5432e85
tothepowerofn <nathanedu@icloud.com>
2019-12-02 19:00:14 -0500

    Revised tests, now with scattering

--- tests/data_structures.c
+++ tests/data_structures.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int random_int(int min, int max)
+{
+   return min + rand() % (max+1 - min);
+}
+
 struct linked_list *linked_list_append_before(unsigned int value, struct linked_list *head) {
 	struct linked_list *new_head = (struct linked_list*) malloc(sizeof(struct linked_list));
 	printf("pointer to next node: %p\n", new_head);
@@ -24,27 +29,37 @@ struct doubly_linked_list *doubly_linked_list_append_before(unsigned int value, 
 	return new_head;
 }
 
-int main() {
-	for (int i = 0; i < 1; i++) {
+void simple_linked_list_test(int list_size, int num_lists){
+	for (int i = 0; i < num_lists; i++) {
 		struct linked_list *list = NULL;
-		for (int j = 0; j < 10; j++) {
+		for (int j = 0; j < list_size; j++) {
 			list = linked_list_append_before(66, list);
 		}
 	}
+}
 
-	/*for (int i = 0; i < 10; i++) {
+//freq_bogus is a parameter in [1,inf) specifying how often to bogus nodes. Greater = more bogus
+void scattered_linked_list(int list_size, int num_lists, int freq_bogus){
+	if(freq_bogus < 1){
+		printf("Bogus frequency must be greater than 1!\n");
+	}
+	for (int i = 0; i < num_lists; i++) {
 		struct linked_list *list = NULL;
-		for (int j = 0; j < 100; j++) {
-			list = linked_list_append_before(rand() % 100, list);
+		for (int j = 0; j < list_size; j++) {
+			if(random_int(0,1) == 1){
+				list = linked_list_append_before(66, list);
+			}else{
+				struct linked_list *new_head = (struct linked_list*) malloc(sizeof(struct linked_list)); //bogus allocation
+				new_head->value = 10; // avoid optimization out
+				--j; // we made a bogus node, so it shouldn't count towards our 10
+				printf("pointer to BOGUS node: %p\n", new_head);
+			}
 		}
-	}*/
+	}
+}
 
-	/*for (int i = 0; i < 10; i++) {
-		struct doubly_linked_list *list = NULL;
-		for (int j = 0; j < 100; j++) {
-			list = doubly_linked_list_append_before(rand() % 100, list);
-		}
-	}*/
-
+int main() {
+	//Make a linked list with non-consecutive nodes
+	scattered_linked_list(20,1,3);
 	sleep(10000);
 }
